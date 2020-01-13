a5b096f3f5c5f9d64d5c30ca585f3923764bff56
yangalli <yangalli>
2020-01-12 21:58:28 -0300

    feat: config rspec

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,15 @@ module DesafioTrofeus
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
